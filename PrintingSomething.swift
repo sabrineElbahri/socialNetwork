749c7df3840a744e68b0835e7b84f70fe2e5259d
Sabrine Elbahri <sabrine.elbahri@3ie.fr>
2016-09-09 18:56:48 +0200

    adding a swift file for test

---
+++ PrintingSomething.swift
@@ -0,0 +1,20 @@
+//
+//  PrintingSomething.swift
+//  
+//
+//  Created by Sabrine Elbahri on 09/09/16.
+//
+//
+
+import Foundation
+import UIKit
+import Bolts
+import FacebookLogin
+import FacebookCore
+import FacebookShare
+
+public class PrintingSomething {
+    public func youhou() {
+        print("social network")
+    }
+}
