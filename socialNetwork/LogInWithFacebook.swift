7d30dc8181f9a34cd7a76be73aa8d2d76b56d69c
Sabrine Elbahri <sabrine.elbahri@3ie.fr>
2016-09-12 16:26:06 +0200

    more functions in LogInWithFacebook

--- socialNetwork/LogInWithFacebook.swift
+++ socialNetwork/LogInWithFacebook.swift
@@ -13,6 +13,13 @@ import FacebookCore
 import FacebookLogin
 
 public class LogInWithFacebook {
+    
+    private var userId: AnyObject?
+    private var userEmail: AnyObject?
+    private var userFirstName: AnyObject?
+    private var userLastName: AnyObject?
+    
+    
     public init() {}
     
     public func login(vc: UIViewController) {
@@ -41,10 +48,25 @@ public class LogInWithFacebook {
                 graphRequest.start { (httpResponse, result) -> Void in
                     switch result {
                     case .Success(let response):
+                        if let userId: AnyObject = response.dictionaryValue!["id"] {
+                            self.userId = userId
+                        }
+                        if let userEmail: AnyObject = response.dictionaryValue!["email"] {
+                            self.userEmail = userEmail
+                        }
+                        
+                        if let userFirstName: AnyObject = response.dictionaryValue!["first_name"] {
+                            self.userFirstName = userFirstName
+                        }
+                        
+                        if let userLastName: AnyObject = response.dictionaryValue!["last_name"] {
+                            self.userLastName = userLastName
+                        }
+                        
                         debugPrint("Graph Request Succeeded: \(response)")
                         debugPrint("dico : \(response.dictionaryValue)")
-                        debugPrint("string : \(response.stringValue)")
-                        debugPrint("array : \(response.arrayValue)")
+
+                        
                     case .Failed(let error):
                         debugPrint("Graph Request Failed: \(error)")
                     }
@@ -54,21 +76,56 @@ public class LogInWithFacebook {
             }
         }
     }
+
+    
+    public func getFacebookId() -> String {
+        return userId as! String
+    }
+    
+    public func getFacebookEmail() -> String {
+        return userEmail as! String
+    }
+    
+    public func getFacebookFirstName() -> String {
+        return userFirstName as! String
+    }
+    
+    public func getFacebookLastName() -> String {
+        return userLastName as! String
+    }
+    
+
+    public func getFacebookProfileImageUrl(facebookId: Int?, profileImage: UIImage) -> UIImage? {
+        if facebookId != nil {
+            let url =  NSURL(string: "http://graph.facebook.com/\(facebookId!)/picture?type=normal")
+            
+                let data = NSData(contentsOfURL: url!)
+            
+                    if data != nil {
+                        return UIImage(data: data!)
+                    }
+        }
+        
+        return nil
+    }
+    
+    public func getFacebookProfileImageUrlAsync(facebookId: Int?, completed: (image: UIImage) -> Void) -> Void {
+        if facebookId != nil {
+            let url =  NSURL(string: "http://graph.facebook.com/\(facebookId!)/picture?type=normal")
+                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+                    let data = NSData(contentsOfURL: url!)
+                    
+                    if data != nil {
+                        completed(image: UIImage(data: data!)!)
+                    }
+            }
+        }
+    }
     
 }
 
 
-/*public func getFacebookProfileImageUrl(facebookId: Int?) -> UIImageView? {
-        if facebookId != nil {
 
-        } else {
-            return nil
-        }
-    
-} */
-
-        
-        
         
         
         
